a2bac1d8e6d37c675f956746e25907a60025d4a3
Kaklyi <karelyttrevino@gmail.com>
2026-01-18 01:02:24 -0600

    Create Module1_Option1.java

---
+++ Module1_Option1.java
@@ -0,0 +1,17 @@
+void main(){
+    {
+//Make individual values for: First name, Last name, Street address, City, Zip code
+        String firstName = "Lilieth";
+        String lastName = "Gomez";
+        String streetAddress = "123 West Kings Hwy";
+        String city = "Austin";
+        String zipCode = "78212";
+//print out the individual lines
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("ZipCode: " + zipCode);
+    }
+}
+
